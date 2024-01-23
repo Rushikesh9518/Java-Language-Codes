f9d57ab5d53688c3067f246e2f1fa2257ea6d5c8
Rushikesh9518 <157128902+Rushikesh9518@users.noreply.github.com>
2024-01-23 23:11:08 +0530

    Add files via upload

---
+++ ArrayByUser.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class ArrayByUser {
+    static void printArray(int arr[]) {
+        System.out.println("Array Elements are :");
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(+arr[i]);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Array Size ");
+
+        int arrSize = sc.nextInt();
+
+        int[] userArray = new int[arrSize];
+
+        for (int i = 0; i < arrSize; i++) {
+            System.out.println("Enter Array Elements " + (i + 1) + " :");
+            userArray[i] = sc.nextInt();
+        }
+        printArray(userArray);
+
+    }
+
+}
