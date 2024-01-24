fb9370e227cac426378e10a4b3fe617967a7c22e
Rushikesh9518 <157128902+Rushikesh9518@users.noreply.github.com>
2024-01-24 18:27:45 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    static void fibonacciSeries(int num) {
+        int firstTerm = 0;
+        int secondTerm = 1;
+        for (int i = 1; i <= num; i++) {
+            System.out.print(+firstTerm + " ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Fibonacci Series upto : ");
+        int num = sc.nextInt();
+
+        fibonacciSeries(num);
+
+        sc.close();
+    }
+}
