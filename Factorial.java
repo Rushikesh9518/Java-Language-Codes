6a0e7da92c9708881a0766c048583129416d8529
Rushikesh9518 <157128902+Rushikesh9518@users.noreply.github.com>
2024-01-24 23:34:21 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Factorial {
+
+    static int factorial(int num) {
+
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Number To Get it's Factorial : ");
+
+        int num = scanner.nextInt();
+        factorial(num);
+
+        scanner.close();
+    }
+}
