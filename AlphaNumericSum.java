bf8a93553067fcd7bab35d464a5dfc903ecb5ab2
Rushikesh9518 <rushikeshshinde.work@gmail.com>
2024-12-17 23:54:59 +0530

    String

---
+++ AlphaNumericSum.java
@@ -0,0 +1,27 @@
+package Java;
+
+import java.util.Scanner;
+
+public class AlphaNumericSum {
+
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the alpha numeric string");
+		String str=sc.nextLine();
+		
+		char[] ch=str.toCharArray();
+		
+		int sum=0;
+		for(int i=0;i<ch.length;i++)
+		{
+			if(ch[i]>=48 && ch[i]<=57)
+			{
+				sum=sum+(ch[i]-48);
+			}
+		}
+		System.out.println(sum);
+	}
+}
