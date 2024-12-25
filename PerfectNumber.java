af5291a9307063889796993f605d234dbf669289
Rushikesh9518 <rushikeshshinde.work@gmail.com>
2024-12-25 23:54:45 +0530

    Number

---
+++ PerfectNumber.java
@@ -0,0 +1,28 @@
+package Java;
+import java.util.*;
+
+public class PerfectNumber {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("enter a number");
+		int num=sc.nextInt();
+
+		int sum=1;
+		for (int i=2;i<=num/2;i++ ){
+
+			if (num%i==0)
+				sum=sum+i;
+
+		}if (sum==num){
+
+			System.out.println(num+" is a Perfect number");
+
+		}else {
+
+			System.out.println(num+" is not a Perfect number");
+		}
+	}
+}
